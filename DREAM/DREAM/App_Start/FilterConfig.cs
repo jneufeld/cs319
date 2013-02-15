4a0ad11f040c20a70f2e9146981e27d640ffee42
Steven Baldwin <locke9@gmail.com>
2013-02-15 14:18:35 -0800

    Create ASP.NET MVC 4 template project.

---
+++ DREAM/DREAM/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DREAM
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
