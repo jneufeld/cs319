3cff50b99c7363541abcf92c8c8c725574941c2c
David Yuckin <dyuckin@gmail.com>
2013-02-18 18:28:23 -0800

    Add an empty custom membership provider and configure the project to use it

---
+++ DREAM/DREAM/CustomMembership/DREAMMembershipProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace DREAM.CustomMembership
+{
+    public class DREAMMembershipProvider : SqlMembershipProvider
+    {
+    }
+}
