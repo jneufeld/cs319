e824464059604fa651ae10ffbb0b261a8b5a116b
Jessica Wong <jessicawong16@gmail.com>
2013-03-02 16:38:58 -0800

    Extracted class declarations from UserModels.cs into separate files.

---
+++ DREAM/DREAM/Models/UserProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace DREAM.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+
+}
