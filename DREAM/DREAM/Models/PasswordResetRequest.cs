8bef1c23504a54463e8d6fca8f7b0165a0238d63
jkay008 <jk.killow@gmail.com>
2013-03-03 16:05:21 -0800

    Added PasswordResetRequest model.
    Still have to implement sending an email to the user.

---
+++ DREAM/DREAM/Models/PasswordResetRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace DREAM.Models
+{
+    public class PasswordResetRequest
+    {
+        public long ID;
+	    [Required]
+	    public MembershipUser User;
+	    //[DatabaseGenerated(DatabaseGenerationOption.Computed)]
+	    public DateTime timestamp;
+	    public bool Enabled = true;
+	
+	    public static long GenerateNewID()
+        {
+		    byte[] buffer = Guid.NewGuid().ToByteArray();
+		    long id = BitConverter.ToInt64(buffer, 0);
+		   // if(another PasswordResetRequests have the same id)
+			 //   id = GenerateNewID();
+		return id;
+        }
+
+	    public static PasswordResetRequest GenerateFor(MembershipUser user){
+		    PasswordResetRequest resetReq = null;
+		    using(DREAMContext db = new DREAMContext()) {
+			    resetReq = new PasswordResetRequest();
+			    {
+				    resetReq.ID = PasswordResetRequest.GenerateNewID();
+				    resetReq.User = user;
+			    }
+			    db.SaveChanges();
+			   // send email to user with a link to the PasswordResetRequest’s page;
+		    return resetReq;
+            }
+         }
+    }
+}
