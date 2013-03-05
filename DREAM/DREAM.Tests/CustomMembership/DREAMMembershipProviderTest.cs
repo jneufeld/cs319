b1df4af565b3949e4a6d0159fa68480b595eba54
David Yuckin <dyuckin@gmail.com>
2013-03-04 23:11:56 -0800

    Override the ChangePassword method of DREAMMembershipProvider

--- DREAM/DREAM.Tests/CustomMembership/DREAMMembershipProviderTest.cs
+++ DREAM/DREAM.Tests/CustomMembership/DREAMMembershipProviderTest.cs
@@ -1,30 +1,66 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DREAM.CustomMembership;
 using System.Web.Security;
+using DREAM.Models;
 
 namespace DREAM.Tests.CustomMembership
 {
     [TestClass]
     public class DREAMMembershipProviderTest
     {
-        [TestMethod]
-        public void ExtractPasswordData()
+        MembershipUser user;
+
+        [TestInitialize]
+        public void SetUpData()
         {
             // if the test user already exists delete it
             Membership.DeleteUser("user1");
 
-            MembershipUser user = Membership.CreateUser("user1", "Password1!", "user1@example.com");
+            user = Membership.CreateUser("user1", "Password1!", "user1@example.com");
+        }
 
-            Assert.IsNotNull(user);
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Membership.DeleteUser("user1");
+        }
 
+        [TestMethod]
+        public void ExtractPasswordData()
+        {
             Tuple<MembershipPasswordFormat, string, string> passwordData = DREAMMembershipProvider.ExtractPasswordData(user);
 
             Assert.AreEqual(passwordData.Item1, MembershipPasswordFormat.Hashed);
             Assert.IsNotNull(passwordData.Item2);
             Assert.IsNotNull(passwordData.Item3);
+        }
 
-            Membership.DeleteUser("user1");
+        [TestMethod]
+        public void CreateUser()
+        {
+            using (DREAMContext db = new DREAMContext())
+            {
+                Assert.AreEqual(1, db.PreviousPasswords.Where(p => p.UserID.Equals((Guid)user.ProviderUserKey)).Count());
+            }
+        }
+
+        [TestMethod]
+        public void ChangePassword()
+        {
+            Assert.IsTrue(user.ChangePassword("Password1!", "NewPassword1!"));
+            try
+            {
+                user.ChangePassword("NewPassword1!", "Password1!");
+                Assert.Fail("ChangePassword didn't through an PreviouslyUsedPasswordException.");
+            }
+            catch (PreviouslyUsedPasswordException) { }
+            catch (Exception)
+            {
+                Assert.Fail("ChangePassword didn't through an PreviouslyUsedPasswordException.");
+            }
         }
     }
 }
