7ca5d1643aa75f0be4716ded58e32ee8f168bb8e
jkay008 <jk.killow@gmail.com>
2013-03-05 17:59:18 -0800

    Test class for PasswordResetRequest

---
+++ DREAM/DREAM.Tests/Models/PasswordResetRequestTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DREAM.Models;
+
+namespace DREAM.Tests.Models
+{
+    [TestClass]
+    public class PasswordResetRequestTest
+    {
+        [TestMethod]
+        public void GenerateNewIDTest()
+        {
+            Assert.IsNotNull(PasswordResetRequest.GenerateNewID(), "ID should never be null", null);
+        }
+    }
+}
