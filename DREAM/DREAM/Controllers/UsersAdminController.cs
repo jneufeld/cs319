de2a1a3f7bc7ae79546b87be874ac1498e03011f
David Yuckin <dyuckin@gmail.com>
2013-03-06 14:13:05 -0800

    Add and implement the main users administration page

---
+++ DREAM/DREAM/Controllers/UsersAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace DREAM.Controllers
+{
+    public class UsersAdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            MembershipUserCollection users = Membership.GetAllUsers();
+            return View(users);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(string username)
+        {
+        }
+    }
+}
