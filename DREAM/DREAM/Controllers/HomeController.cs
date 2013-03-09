f6712efa1367f5eadc94fc8bf9544f5c21e49d3d
Jessica Wong <jessicawong16@gmail.com>
2013-03-09 13:08:30 -0800

    Preliminary modifications to DREAM UI.  Just modified the Home Page (pre-login), About, and Contact pages.

--- DREAM/DREAM/Controllers/HomeController.cs
+++ DREAM/DREAM/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DREAM.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Please login to start using the system.";
 
             return View();
         }
