9e6e180f5b1bc61bfc941e281a9d1649140e1aec
jkay008 <jk.killow@gmail.com>
2013-03-10 21:13:48 -0700

    Added password models.

---
+++ DREAM/DREAM/Models/ResetPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DREAM.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string NewPassword2 { get; set; }
+    }
+}
