f7a7fb6c81d39f437ec0b3d58bfa307c51ce74e3
jkay008 <jk.killow@gmail.com>
2013-03-10 19:34:01 -0700

    Update and tested DropDownAdminController with cshtml files added.

--- DREAM/DREAM/Controllers/DropDownAdminController.cs
+++ DREAM/DREAM/Controllers/DropDownAdminController.cs
@@ -17,81 +17,173 @@ namespace DREAM.Controllers
         private DREAMContext db = new DREAMContext();
 
         [HttpGet]
-        public ActionResult Index(string dropDownClass) {
+        public ActionResult Index(string dropDownClass)
+        {
             DbSet dropDowns;
-		    dropDowns = getDropDowns(dropDownClass);
+            dropDowns = getDropDowns(dropDownClass);
             return View(dropDowns);
         }
 
-	    [HttpGet]
-	        public ActionResult Add(string dropDownClass){
-		        return View();
-        }
-
-	    [HttpPost]
-	    [ValidateAntiForgeryToken]
-	    public ActionResult Add(DropDown model, string dropDownClass) {
-		    if(ModelState.IsValid) {
-                DbSet dropDowns;
-                dropDowns = getDropDowns(dropDownClass);
-		        dropDowns.Add(model);
-			    db.SaveChanges();
-                RouteValueDictionary routes = new RouteValueDictionary();
-                routes.Add("id", model.ID);
-                routes.Add("dropDownClass", dropDownClass);
-                return RedirectToAction("Edit", "DropDownAdminController", routes);
-            }
-            else {
-                ModelState.AddModelError(model.ToString(), "ModelState is not valid");
-                return View(model);
-            }
-        }
         [HttpGet]
-	    public ActionResult Edit(int id, string dropDownClass) {
-            DbSet dropDowns;
-            DropDown dropDown;
-		    dropDowns = getDropDowns(dropDownClass);
-		    dropDown = (DropDown)dropDowns.Find(id);
-            return View(dropDown);
+        public ActionResult Add(string dropDownClass)
+        {
+            DropDown m = null;
+            switch (dropDownClass)
+            {
+                case "RequestType":
+                    m = new RequestType();
+                    break;
+                case "QuestionType":
+                    m = new QuestionType();
+                    break;
+                case "TumourGroup":
+                    m = new TumourGroup();
+                    break;
+                case "Regions":
+                    m = new Region();
+                    break;
+            }
+            return View(m);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(DropDown model, string dropDownClass){
-            if(ModelState.IsValid) {
+        public ActionResult Add(DropDown model, string dropDownClass)
+        {
+            DropDown m = null;
+            switch (dropDownClass)
+            {
+                case "RequestType":
+                    m = new RequestType();
+                    break;
+                case "QuestionType":
+                    m = new QuestionType();
+                    break;
+                case "TumourGroup":
+                    m = new TumourGroup();
+                    break;
+                case "Regions":
+                    m = new Region();
+                    break;
+            }
+            m.Code = model.Code;
+            m.FullName = model.FullName;
+            if (ModelState.IsValid)
+            {
+                DbSet dropDowns;
+                dropDowns = getDropDowns(dropDownClass);
+                dropDowns.Add(m);
                 db.SaveChanges();
                 RouteValueDictionary routes = new RouteValueDictionary();
-                routes.Add("id", model.ID);
+                routes.Add("dropDownId", m.ID);
                 routes.Add("dropDownClass", dropDownClass);
-                return RedirectToAction("Edit", "DropDownAdminController", routes);
+                return RedirectToAction("Edit", "DropDownAdmin", routes);
             }
-	        else {
-                ModelState.AddModelError(model.ToString(),"ModelState is not valid");
-	            return View(model);
+            else
+            {
+                ModelState.AddModelError(m.FullName, "ModelState is not valid");
+                return View(m);
             }
+        }
+        [HttpGet]
+        public ActionResult Edit(int dropDownId, string dropDownCode, string dropDownFullName, string dropDownClass)
+        {
+            DropDown m = null;
+            switch (dropDownClass)
+            {
+                case "RequestType":
+                    m = new RequestType();
+                    break;
+                case "QuestionType":
+                    m = new QuestionType();
+                    break;
+                case "TumourGroup":
+                    m = new TumourGroup();
+                    break;
+                case "Regions":
+                    m = new Region();
+                    break;
+            }
+            m.ID = dropDownId;
+            m.Code = dropDownCode;
+            m.FullName = dropDownFullName;
+            return View(m);
         }
 
         [HttpPost]
-        public ActionResult Delete(int id, string dropDownClass) {
-            DbSet dropDowns;
-            DropDown dropDown;
-	        dropDowns = getDropDowns(dropDownClass);
-	        dropDown = (DropDown)dropDowns.Find(id);
-	        dropDowns.Remove(dropDown);
-            RouteValueDictionary routes = new RouteValueDictionary();
-            routes.Add("dropDownClass", dropDownClass);
-            return RedirectToAction("Index", "DropDownAdminController", routes);
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(DropDown model, string dropDownClass)
+        {
+            if (ModelState.IsValid)
+            {
+                DbSet dropDowns;
+                DropDown dropDown = new DropDown();
+                dropDowns = getDropDowns(dropDownClass);
+                dropDown = (DropDown)dropDowns.Find(model.ID);
+                dropDown.Code = model.Code;
+                dropDown.FullName = model.FullName;
+                db.SaveChanges();
+                RouteValueDictionary routes = new RouteValueDictionary();
+                routes.Add("dropDownClass", dropDownClass);
+                return RedirectToAction("Index", "DropDownAdmin", routes);
+            }
+            else
+            {
+                ModelState.AddModelError(model.ToString(), "ModelState is not valid");
+                return View(model);
+            }
         }
 
-	    private DbSet getDropDowns(string dropDownClass)
+        [HttpGet]
+        public ActionResult Delete(int dropDownId, string dropDownCode, string dropDownFullName, string dropDownClass)
         {
-		    switch(dropDownClass) {
-			    case "RequestType":
+            DropDown m = null;
+            switch (dropDownClass)
+            {
+                case "RequestType":
+                    m = new RequestType();
+                    break;
+                case "QuestionType":
+                    m = new QuestionType();
+                    break;
+                case "TumourGroup":
+                    m = new TumourGroup();
+                    break;
+                case "Regions":
+                    m = new Region();
+                    break;
+            }
+            m.ID = dropDownId;
+            m.Code = dropDownCode;
+            m.FullName = dropDownFullName;
+            return View(m);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int dropDownId, string dropDownClass)
+        {
+            DbSet dropDowns;
+            DropDown dropDown = new DropDown();
+            dropDowns = getDropDowns(dropDownClass);
+            dropDown = (DropDown)dropDowns.Find(dropDownId);
+            dropDowns.Remove(dropDown);
+            db.SaveChanges();
+            RouteValueDictionary routes = new RouteValueDictionary();
+            routes.Add("dropDownClass", dropDownClass);
+            return RedirectToAction("Index", "DropDownAdmin", routes);
+        }
+
+        private DbSet getDropDowns(string dropDownClass)
+        {
+            switch (dropDownClass)
+            {
+                case "RequestType":
                     return db.RequestTypes;
-			    case "QuestionType":
+                case "QuestionType":
                     return db.QuestionTypes;
-			    case "TumourGroup":
-				    return db.TumourGroups;
+                case "TumourGroup":
+                    return db.TumourGroups;
                 case "Regions":
                     return db.Regions;
             }
