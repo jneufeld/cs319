c476fdef1f0a662d3b0d69e7c27ecc9914f17dc0
David Yuckin <dyuckin@gmail.com>
2013-03-11 14:19:31 -0700

    Fix index page test

--- DREAM/DREAM.Tests/Controllers/HomeControllerTest.cs
+++ DREAM/DREAM.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace DREAM.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Please login to start using the system.", result.ViewBag.Message);
         }
 
         [TestMethod]
