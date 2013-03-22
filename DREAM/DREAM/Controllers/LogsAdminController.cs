408364f7e9bcae4b064412d1b16314dc0a4924f9
ryantaylor <ryan.taylor9@gmail.com>
2013-03-21 23:02:33 -0700

    Basic filtering for logs, right now only by Request ID

--- DREAM/DREAM/Controllers/LogsAdminController.cs
+++ DREAM/DREAM/Controllers/LogsAdminController.cs
@@ -17,11 +17,37 @@ namespace DREAM.Controllers
         //
         // GET: /LogsAdmin/
 
-        public ActionResult Index(Guid? user, int? request, LogAction? action, DateTime? before, DateTime? after, int page = 1)
+        public ActionResult Index(String request = "null", int page = 1)
         {
-            var logs = db.Logs.OrderByDescending(m => m.ID).ToPagedList(page, 2);
-            ViewBag.logsPage = logs;
-            return View(logs);
+            LogFilterModel lfm = new LogFilterModel();
+
+            ContentResult req = new ContentResult { Content = request, ContentType = "application/json" };
+            if (!req.Content.Equals("null"))
+            {
+                lfm.RequestID = Convert.ToInt32(req.Content);
+                lfm.Logs = lfm.Logs.Where(log => log.RequestID == lfm.RequestID);
+            }
+            lfm.PagedLogs = lfm.Logs.OrderByDescending(m => m.ID).ToPagedList(page, 2);
+            return View(lfm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(String button, LogFilterModel lfm)
+        {
+            if (button.Equals("Filter"))
+            {
+                LogFilterModel filter = lfm;
+
+                if (!filter.RequestID.Equals(null))
+                    filter.Logs = filter.Logs.Where(log => log.RequestID == filter.RequestID);
+
+                filter.PagedLogs = filter.Logs.OrderByDescending(m => m.ID).ToPagedList(1, 2);
+                ViewBag.logsPage = filter.PagedLogs;
+                ViewBag.request = filter.RequestID;
+                return View(filter);
+            }
+            else
+                return View(lfm);
         }
     }
 }
---
+++ DREAM/DREAM/Models/LogFilterModel.cs
@@ -0,0 +1,29 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DREAM.Models
+{
+    public class LogFilterModel
+    {
+        [Display(Name = "Request ID")]
+        public int? RequestID { get; set; }
+        public Guid? UserID { get; set; }
+        public LogAction? Action { get; set; }
+        public DateTime? Before { get; set; }
+        public DateTime? After { get; set; }
+        public IEnumerable<Log> Logs { get; set; }
+        public IPagedList<Log> PagedLogs { get; set; }
+
+        public LogFilterModel()
+        {
+            DREAMContext db = new DREAMContext();
+
+            Logs = db.Logs.ToList();
+        }
+    }
+}
