9336de555e5eba0c4d526e74a433f83fa95e8f73
ryantaylor <ryan.taylor9@gmail.com>
2013-03-24 14:51:13 -0700

    Fixed bug in HomeController, added regex for phone numbers

--- DREAM/DREAM/Models/RequestViewModel.cs
+++ DREAM/DREAM/Models/RequestViewModel.cs
@@ -52,6 +52,7 @@ namespace DREAM.Models
         public string CallerLastName { get; set; }
 
         [Display(Name = "Phone Number")]
+        [RegularExpression(@"^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$", ErrorMessage = "Entered phone format is not valid.")]
         public string CallerPhoneNumber { get; set; }
 
         [Email]
