8295fe1299f21e049f3d69b8903c54fbd792378e
ryantaylor <ryan.taylor9@gmail.com>
2013-04-01 21:52:02 -0700

    Fixed status display on homepage, removed filtering from the DataTable returned in Search.

--- DREAM/DREAM/Models/RequestViewModel.cs
+++ DREAM/DREAM/Models/RequestViewModel.cs
@@ -31,7 +31,7 @@ namespace DREAM.Models
         [Display(Name = "Status")]
         public string Status
         {
-            get { return ClosedBy != null ? "Closed" : "Open"; }
+            get { return !ClosedBy.Equals("") ? "Closed" : "Open"; }
         }
 
         [Display(Name = "Requester Type")]
