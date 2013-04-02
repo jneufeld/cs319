f50c2b92fb34234fc8073c6abca025be1881f827
Jessica Wong <jessicawong16@gmail.com>
2013-04-01 20:46:09 -0700

    Index test for Keyword Admin.

--- DREAM/DREAM.Tests/Controllers/KeywordAdminControllerTest.cs
+++ DREAM/DREAM.Tests/Controllers/KeywordAdminControllerTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DREAM;
@@ -13,27 +13,42 @@ namespace DREAM.Tests.Controllers
     [TestClass]
     public class KeywordAdminControllerTest
     {
+        private  KeywordsAdminController keywordsAdminController = new KeywordsAdminController();
+        private DREAMContext dbContext = new DREAMContext();
 
         [TestMethod]
-        public void Edit(Keyword testKeyword)
+        public void IndexTest()
         {
-            // arrange
-            KeywordsAdminController keywordsAdminController = new KeywordsAdminController();
-            Keyword testKeyword1 = new Keyword
-            {
-                KeywordText = "Test Keyword",
-                Enabled = true,
-            };
-
-            Question testQuestion1 = new Question();
-            testQuestion1.Keywords.Add(testKeyword1);
-            testKeyword1.AssociatedQuestions.Add(testQuestion1);
-
-            // act
-            ViewResult editResult = keywordsAdminController.Edit(testKeyword1) as ViewResult;
-
-            // assert
-            Assert.IsNotNull(editResult);
+            ActionResult index = keywordsAdminController.Index();
+            Assert.AreEqual("System.Web.Mvc.ViewResult", index.GetType().FullName);
         }
+
+        [TestMethod]
+        public void EditTest(String keyword)
+        {
+
+        }
+
+
+        //[TestMethod]
+        //public void Edit(Keyword testKeyword)
+        //{
+        //    // arrange
+        //    Keyword testKeyword1 = new Keyword
+        //    {
+        //        KeywordText = "Test Keyword",
+        //        Enabled = true,
+        //    };
+
+        //    Question testQuestion1 = new Question();
+        //    testQuestion1.Keywords.Add(testKeyword1);
+        //    testKeyword1.AssociatedQuestions.Add(testQuestion1);
+
+        //    // act
+        //    ViewResult editResult = keywordsAdminController.Edit(testKeyword1) as ViewResult;
+
+        //    // assert
+        //    Assert.IsNotNull(editResult);
+        //}
     }
 }
